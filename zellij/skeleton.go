c57505835eb73dcdec0f91ef5baced459b6292df
Edgar Bering <ebering@uwaterloo.ca>
2011-06-29 15:02:38 -0400

    do skeletons correctly (translate figures/order was wrong), and set active faces correctly

--- zellij/skeleton.go
+++ zellij/skeleton.go
@@ -2,17 +2,30 @@ package zellij
 
 import "strconv"
 import "os"
+import "../quadratic/quadratic"
+import "strings"
 
 func SkeletonMap(spec string) (*quadratic.Map,os.Error) {
-	ret := TileMap(Tiles[0],0)
+	var ret *quadratic.Map
 	currentPoint := quadratic.PointMustFromString("0,0,0,0")
 	origin := quadratic.PointMustFromString("0,0,0,0")
-	for _,d := range(spec) {
-		translatePoint := quadratic.PointMustFromString("4,4,0,0")
-		translateSaft := quadratic.PointMustFromString("0,2,0,0")
-		saft := TileMap(Tiles[0],0)
+	for _,d := range(strings.Split(spec,"",-1)) {
+		translatePoint := quadratic.PointMustFromString("8,4,0,0")
+		translateSaft := quadratic.PointMustFromString("4,2,0,0")
+		saft := TileMap(Tiles[1],0)
 		seal := TileMap(Tiles[0],0)
-		heading := strconv.Atoi(d)
+		heading,_ := strconv.Atoi(d)
+
+		seal.Translate(quadratic.NewVertex(origin),quadratic.NewVertex(currentPoint))
+		if ret == nil {
+			ret = seal
+		} else {
+			r,ok := ret.Overlay(seal,Overlay)
+			if ok != nil {
+				return nil,ok
+			}
+			ret = r
+		}
 
 		translatePoint.RotatePi4(heading)
 		translateSaft.RotatePi4(heading)
@@ -20,20 +33,20 @@ func SkeletonMap(spec string) (*quadratic.Map,os.Error) {
 		saft.Translate(quadratic.NewVertex(origin),quadratic.NewVertex(translateSaft))
 		saft.Translate(quadratic.NewVertex(origin),quadratic.NewVertex(currentPoint))
 	
-		ret,ok = ret.Overlay(saft)
+		r,ok := ret.Overlay(saft,Overlay)
+		ret = r
 		if ok != nil {
 			return nil,ok
 		}
 
-		
-		currentPoint = MakeTranslation(origin,translatePoint)(currentPoint)
-		seal.Translate(quadratic.NewVertex(origin),quadratic.NewVertex(currentPoint))
-
-		ret,ok = ret.Overlay(seal)
-		if ok != nil {
-			return nil,ok
-		}
+		currentPoint = quadratic.MakeTranslation(origin,translatePoint)(currentPoint)
 	}
+
+	ret.Faces.Do(func (f interface{}) {
+		if f.(*quadratic.Face).Value.(string) == "outer" && f.(*quadratic.Face).Inner() {
+			f.(*quadratic.Face).Value = "active"
+		}
+	})
 
 	return ret,nil
 }
