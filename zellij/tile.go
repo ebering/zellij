c7208c5b6b46afe47f1b351e11afe9d073905d91
Edgar Bering <ebering@uwaterloo.ca>
2011-07-11 13:27:28 -0400

    move tile creation code to correct place

--- zellij/tile.go
+++ zellij/tile.go
@@ -16,3 +16,28 @@ func GenerateOrbits(tile *quadratic.Map) []*quadratic.Map {
 	}
 	return orbits
 }	
+
+func TileMap(s string,Generation int) *quadratic.Map {
+	tilePoints := make([]*quadratic.Point, len(s))
+	for i, c := range s {
+		tilePoints[i] = Points[string(c)].Copy()
+	}
+	tile := quadratic.PolygonMap(tilePoints)
+	tile.Edges.Do(func (f interface{}) {
+		f.(*quadratic.Edge).Generation = Generation
+	})
+	tile.Faces.Do(func (f interface{}) {
+		if f.(*quadratic.Face).Value.(string) == "inner" {
+			f.(*quadratic.Face).Type = s
+		}
+	})
+	return tile
+}
+
+func PathMap(s string) *quadratic.Map {
+	tilePoints := make([]*quadratic.Point, len(s))
+	for i, c := range s {
+		tilePoints[i] = Points[string(c)].Copy()
+	}
+	return quadratic.PathMap(tilePoints)
+}
