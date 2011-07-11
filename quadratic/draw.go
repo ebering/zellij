0d3b87c6a6a3b91a14899580ebe96ed1a40a3469
Edgar Bering <ebering@uwaterloo.ca>
2011-07-11 13:24:05 -0400

    colour maps with procedural brushes

--- quadratic/draw.go
+++ quadratic/draw.go
@@ -25,6 +25,17 @@ func (l *Line) Draw(ctx *cairo.Surface) {
 func (m *Map) DrawEdges(ctx *cairo.Surface) {
 	m.Edges.Do(func (f interface{} ) {
 		e,_ := f.(*Edge)
+		e.Line().Draw(ctx)
+	})
+	m.Verticies.Do(func (v interface{}) {
+		ctx.SetSourceRGBA(0.,0.,0.,1.)
+		v.(*Vertex).Draw(ctx)
+	})
+}
+
+func (m *Map) DrawDebugEdges(ctx *cairo.Surface) {
+	m.Edges.Do(func (f interface{} ) {
+		e,_ := f.(*Edge)
 		ctx.SetSourceRGBA(0.,float64((e.Generation*20)%255)/255.,0.,1.)
 		e.Line().Draw(ctx)
 	})
@@ -34,7 +45,7 @@ func (m *Map) DrawEdges(ctx *cairo.Surface) {
 	})
 }
 
-func (m *Map) ColourFaces(ctx *cairo.Surface)  {
+func (m *Map) ColourDebugFaces(ctx *cairo.Surface)  {
 	n := float64(m.Faces.Len())
 	i := 0.
 	m.Faces.Do(func (f interface{}) {
@@ -55,3 +66,17 @@ func (m *Map) ColourFaces(ctx *cairo.Surface)  {
 		i = i+1.
 	})
 }	
+
+func (m *Map) ColourFaces(ctx *cairo.Surface, brush func (*Face) (float64,float64,float64,float64) ) {
+	m.Faces.Do(func (f interface {}) {
+		F := f.(*Face)
+		ctx.SetSourceRGBA(brush(F))
+		e := F.boundary;
+		ctx.MoveTo(e.start.Float64())
+		for f:= e.Next(); f != e; f = f.Next() {
+			ctx.LineTo(f.start.Float64())
+		}
+		ctx.ClosePath()
+		ctx.Fill()
+	})
+}
