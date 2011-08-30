81e6284c650dc40b2c76113b24c958e0d2f169ee
Edgar Bering <ebering@csclub.uwaterloo.ca>
2011-08-30 17:15:45 -0400

    Add json marshalling and unmarshalling for quadratic maps so we can save zellij tilings

---
+++ quadratic/json.go
@@ -0,0 +1,120 @@
+package quadratic
+
+import (
+	"json"
+	"os"
+	"strconv"
+	"fmt"
+	"container/vector"
+)
+
+type mapJSON struct {
+	Verts []*Vertex
+	Edges []edgeJSON
+	Faces []faceJSON
+}
+
+type edgeJSON struct {
+	Start,End string
+}
+
+type faceJSON struct {
+	Start,End string
+	Value interface{}
+	Type string
+}
+
+type intJSON struct {
+	A,B string
+}
+
+func (p *Point) MarshalJSON() ([]byte,os.Error) {
+	x,ex := json.Marshal(p.x)
+	if ex != nil {
+		return nil,ex
+	}
+	y,ey := json.Marshal(p.y)
+	if ey !=nil {
+		return nil,ey
+	}
+	return []byte(fmt.Sprintf("{\"x\":%s,\"y\":%s}",x,y)),nil
+}
+
+func (i *Integer) MarshalJSON() ([]byte,os.Error) {
+	return json.Marshal(intJSON{strconv.Itoa64(i.a),strconv.Itoa64(i.b)})
+}
+
+func (m *Map) MarshalJSON() ([]byte,os.Error) {
+	jv := new(mapJSON)
+	jv.Verts = make([]*Vertex,m.Verticies.Len())
+	jv.Edges = make([]edgeJSON,0,m.Edges.Len()/2)
+	jv.Faces = make([]faceJSON,0,m.Faces.Len())
+	vertindex := make(map[*Vertex]string)
+	for i:=0; i < m.Verticies.Len(); i++ {
+		jv.Verts[i] = m.Verticies.At(i).(*Vertex)
+		vertindex[m.Verticies.At(i).(*Vertex)] = strconv.Itoa(i)
+	}
+	m.Edges.Do(func (e interface{}) {
+		E := e.(*Edge)
+		if E.start.Less(E.end) {
+			jv.Edges = append(jv.Edges,edgeJSON{vertindex[E.start],vertindex[E.end]})
+		}
+	})
+	m.Faces.Do(func (f interface{}) {
+		jv.Faces = append(jv.Faces,faceJSON{vertindex[f.(*Face).boundary.start],vertindex[f.(*Face).boundary.end],f.(*Face).Value,f.(*Face).Type})
+	})
+	return json.Marshal(jv)
+}
+
+func (m *Map) UnmarshalJSON(js []byte) os.Error {
+	jv := new(mapJSON)
+	err := json.Unmarshal(js,jv)
+	if err != nil {
+		return err
+	}
+	for _,v := range(jv.Verts) {
+		m.Verticies.Push(v)
+	}
+	for _,e := range(jv.Edges) {
+		st,_ := strconv.Atoi64(e.Start)
+		en,_ := strconv.Atoi64(e.End)
+		m.JoinVerticies(jv.Verts[st],jv.Verts[en])
+	}
+	for _,f := range(jv.Faces) {
+		st,_ := strconv.Atoi64(f.Start)
+		en,_ := strconv.Atoi64(f.End)
+		nf := new(Face)
+		nf.fromMap = m
+		nf.Value = f.Value
+		nf.Type = f.Type
+		bdyStart := jv.Verts[st]
+		bdyStart.OutgoingEdges.Do(func (e interface{}) {
+			if e.(*Edge).end == jv.Verts[en] {
+				nf.boundary = e.(*Edge)
+			}
+		})
+		nf.DoEdges(func (e *Edge) {
+			e.face = nf
+		})
+	}
+	m.Init()
+	return nil
+}
+
+func (v *Vertex) UnmarshalJSON(js []byte) os.Error {
+	vtx := make(map[string]intJSON)
+	err := json.Unmarshal(js,&vtx)
+	if err != nil {
+		return err
+	}
+	v.Point = &Point{vtx["x"].Integer(),vtx["y"].Integer()}
+	v.OutgoingEdges = new(vector.Vector)
+	return nil
+}
+
+func (i intJSON) Integer() *Integer {
+	a, _ := strconv.Atoi64(i.A)
+	b, _ := strconv.Atoi64(i.B)
+	return &Integer{a,b}
+}
+
--- quadratic/quadratic.6.do
+++ quadratic/quadratic.6.do
@@ -1,3 +1,3 @@
-DEPS="int.go point.go line.go draw.go string.go map.go poly.go overlay.go"
+DEPS="int.go point.go line.go draw.go string.go map.go poly.go overlay.go json.go"
 redo-ifchange $DEPS
 6g -o $3 $DEPS 1>&2
